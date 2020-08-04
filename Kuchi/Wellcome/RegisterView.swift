61babeacaee011d4ab05a848d632a1500ee59731
JohnKostanov <kostanovd@icloud.com>
2020-08-04 20:20:36 +0200

    add textFields modifiers

--- Kuchi/Wellcome/RegisterView.swift
+++ Kuchi/Wellcome/RegisterView.swift
@@ -18,12 +18,31 @@ struct RegisterView: View {
             
             WellcomeMessageView()
             TextField("Type your name...", text: $name)
+                .textFieldStyle(KuchiTextStyle())
+                .padding()
             
             Spacer()
         }
+        .padding()
         .background(WelcomeBackgroundImage())
     }
 }
+
+struct KuchiTextStyle: TextFieldStyle {
+    public func _body(
+        configuration: TextField<Self._Label>) -> some View {
+        return configuration
+            .padding(EdgeInsets(top: 8, leading: 16, bottom: 8, trailing: 16))
+            .background(Color.white)
+            .overlay(RoundedRectangle(cornerRadius: 8)
+                .stroke(lineWidth: 8)
+                .foregroundColor(.blue)
+            )
+            .shadow(color: Color.gray.opacity(0.4), radius: 3, x: 1, y: 2)
+    }
+    
+}
+
 
 struct RegisterView_Previews: PreviewProvider {
     static var previews: some View {
