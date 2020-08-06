5669f542c0fc0f5637087cd88f5e344f090d1ec7
JohnKostanov <kostanovd@icloud.com>
2020-08-06 15:10:04 +0200

    Add The Congratulations View

--- Kuchi/Practice/CongratulationsView.swift
+++ Kuchi/Practice/CongratulationsView.swift
@@ -29,21 +29,36 @@
 import SwiftUI
 
 struct CongratulationsView: View {
-  let avatarSize: CGFloat = 120
-  let userName: String
-  
-  init(userName: String) {
-    self.userName = userName
-  }
-  
-  var body: some View {
-    EmptyView()
-  }
+    let avatarSize: CGFloat = 120
+    let userName: String
+    @ObservedObject var challengesViewModel = ChallengesViewModel()
+    
+    init(userName: String) {
+        self.userName = userName
+    }
+    
+    var body: some View {
+        VStack {
+            Text("Congratulations!")
+                .font(.title)
+                .foregroundColor(.gray)
+            Text("You’re awesome!")
+              .fontWeight(.bold)
+              .foregroundColor(.gray)
+            Button(action: {
+              self.challengesViewModel.restart()
+            }, label: {
+              Text("Play Again")
+            })
+              .padding(.top)
+
+        }
+    }
 }
 
 struct CongratulationsView_Previews: PreviewProvider {
-  static var previews: some View {
-    CongratulationsView(userName: "Johnny Swift")
-      .environmentObject(ChallengesViewModel())
-  }
+    static var previews: some View {
+        CongratulationsView(userName: "Johnny Swift")
+            .environmentObject(ChallengesViewModel())
+    }
 }
